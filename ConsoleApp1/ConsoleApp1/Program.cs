5174cc5855b4ac3f0b67c8519015b6420cfdbc2a
s14937 <s14937@pjwstk.edu.pl>
2024-03-24 19:00:52 +0100

    Dodano metode avg

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("aww yeah");
+
+static double avg(int[] ints)
+{
+    int sum = 0;
+    for (int i = 0;i<ints.Length;i++)
+    {
+        sum += ints[i];
+    }
+
+    return (sum / Convert.ToDouble(ints.Length));
+}
+
+int[]tabliczka ={0,100,0};
+
+Console.WriteLine(avg(tabliczka));
